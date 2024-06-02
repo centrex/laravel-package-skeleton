77a893b3b060dde0d30f54319a84cb54614aa3d7
rochi88 <raisul.me@gmail.com>
2024-06-02 14:11:14 +0600

    update rector

--- rector.php
+++ rector.php
@@ -4,8 +4,7 @@ declare(strict_types = 1);
 
 use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\{LevelSetList, SetList};
 
 return RectorConfig::configure()
     ->withPaths([
@@ -13,7 +12,7 @@ return RectorConfig::configure()
         __DIR__ . '/tests',
     ])
     ->withSets([
-        LevelSetList::UP_TO_PHP_82,
+        LevelSetList::UP_TO_PHP_83,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
